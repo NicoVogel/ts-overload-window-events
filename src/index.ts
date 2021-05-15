1750daba802b9c227c81d4a02f465bc7414ec45c
Nico Vogel <nico.vogel@capgemini.com>
2021-05-15 17:38:19 +0200

    use declare var for CustomElement

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,6 @@
-import { KnownEvents } from './types';
-import { RequiredProp } from './types/utils';
+import { CustomEvent } from './types/dom';
 
-function createKnownEvent<EventKey extends keyof KnownEvents>(
-  typeArg: EventKey,
-  eventInitDict: RequiredProp<CustomEventInit<KnownEvents[EventKey]>, 'detail'>
-): CustomEvent<KnownEvents[EventKey]> {
-  return new CustomEvent(typeArg, eventInitDict);
-}
-
-// import { CustomEvent } from './types/dom';
-// const event2 = new CustomEvent('test', { detail: [1, 2, 3] });
-
-const event = createKnownEvent('test', { detail: [1, 2, 3] });
-
+const event = new CustomEvent('test', { detail: [1, 2, 3] });
 window.addEventListener('test', (event) => {
   console.log(event.detail);
 });
--- src/types/dom.d.ts
+++ src/types/dom.d.ts
@@ -9,24 +9,13 @@ declare global {
       options?: boolean | AddEventListenerOptions
     ): void;
   }
-  interface CustomEvent {
-    prototype: CustomEvent;
-    new <EventKey extends keyof KnownEvents>(
-      typeArg: EventKey,
-      eventInitDict: RequiredProp<CustomEventInit<KnownEvents[EventKey]>, 'detail'>
-    ): CustomEvent<KnownEvents[EventKey]>;
-    new <Key extends string, Type>(
-      typeArg: ExcludeValues<Key, keyof KnownEvents>,
-      eventInitDict?: CustomEventInit<Type>
-    ): CustomEvent<Type>;
-  }
 }
 
-// export declare var CustomEvent: {
-//   prototype: CustomEvent;
-//   new <EventKey extends keyof Events>(
-//     typeArg: EventKey,
-//     eventInitDict: RequiredProp<CustomEventInit<Events[EventKey]>, 'detail'>
-//   ): CustomEvent<Events[EventKey]>;
-//   new <Key extends string, Type>(typeArg: ExcludeValues<Key, keyof Events>, eventInitDict?: CustomEventInit<Type>): CustomEvent<Type>;
-// };
+export declare var CustomEvent: {
+  prototype: CustomEvent;
+  new <EventKey extends keyof KnownEvents>(
+    typeArg: EventKey,
+    eventInitDict: RequiredProp<CustomEventInit<KnownEvents[EventKey]>, 'detail'>
+  ): CustomEvent<KnownEvents[EventKey]>;
+  new <Key extends string, Type>(typeArg: ExcludeValues<Key, keyof KnownEvents>, eventInitDict?: CustomEventInit<Type>): CustomEvent<Type>;
+};
